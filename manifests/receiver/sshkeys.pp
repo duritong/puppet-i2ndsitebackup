84df37e0bbd3c4d8035db6350fa1ab6cf13aa315
mh <mh@immerda.ch>
2011-04-26 22:23:42 +0200

    add receiver configuration

---
+++ manifests/receiver/sshkeys.pp
@@ -0,0 +1,9 @@
+class i2ndsitebackup::receiver::sshkeys {
+  $ssh_keys = ssh_keygen("${$i2ndsitebackup::receiver::ssh_key_basepath}/i2ndsitebackup/keys/${i2ndsitebackup::receiver::source_host}/duplicity")
+
+  sshd::ssh_authorized_key{'sndsite':
+    type => 'ssh-rsa',
+    user => 'sndsite',
+    key => $ssh_keys[1],
+  }
+}
