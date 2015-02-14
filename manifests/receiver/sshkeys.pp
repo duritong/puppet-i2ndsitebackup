c04e717c1f0ad91e3cd7a4192505f6a105520be2
mh <mh@immerda.ch>
2015-02-14 13:20:01 +0100

    use the simplified define

--- manifests/receiver/sshkeys.pp
+++ manifests/receiver/sshkeys.pp
@@ -2,8 +2,7 @@
 class i2ndsitebackup::receiver::sshkeys {
   $ssh_keys = ssh_keygen("${$i2ndsitebackup::receiver::ssh_key_basepath}/i2ndsitebackup/keys/${i2ndsitebackup::receiver::source_host}/duplicity")
 
-  sshd::ssh_authorized_key{'sndsite':
-    type  => 'ssh-rsa',
+  sshd::authorized_key{'sndsite':
     user  => 'sndsite',
     key   => $ssh_keys[1],
   }
