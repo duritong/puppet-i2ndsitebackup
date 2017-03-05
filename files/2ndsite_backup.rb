7ef796f968e01739a2d2e407e97229d34eff19ea
mh <mh@immerda.ch>
2017-03-05 21:00:06 +0100

    this bug is gone

--- files/2ndsite_backup.rb
+++ files/2ndsite_backup.rb
@@ -58,22 +58,7 @@ class DuplicityRunner
     ssh_port ||= '22'
     td = File.join(options['target_root'],target)
     ts = "ssh://#{tu}@#{th}/#{td}"
-    # TODO:
-    # we might go with the "legacy" pexpect ssh backend as we are hitting a
-    # paramiko bug:
-    # https://github.com/paramiko/paramiko/issues/151
-    # Verify whether this is fixed at some point
-    # 2nd) round: the parmiko bug might have been triggered by a fw rule, where
-    # disabling helped. To be removed once everything seems to work.
-    # However, we can't do it all the time as folders starting with open
-    # will fail on that backend:
-    # https://bugs.launchpad.net/duplicity/+bug/933388
-    if target.split('/').last =~ /^open/
-      du = ''
-    else
-      du = "--ssh-backend pexpect "
-    end
-    du += "--ssh-options '-oIdentityFile=/opt/2ndsite_backup/duplicity_key' --encrypt-key #{options['gpg_key']} --sign-key #{options['gpg_key']}"
+    du = "--ssh-options '-oIdentityFile=/opt/2ndsite_backup/duplicity_key' --encrypt-key #{options['gpg_key']} --sign-key #{options['gpg_key']}"
     [ "ssh -i /opt/2ndsite_backup/duplicity_key -p #{ssh_port} #{tu}@#{ssh_host} 'test -d #{td} || mkdir -p #{td}'",
       "duplicity cleanup #{archive_dir}--extra-clean --force #{du} #{ts}",
       "duplicity remove-all-but-n-full 2 #{archive_dir}--force #{du} #{ts}",
