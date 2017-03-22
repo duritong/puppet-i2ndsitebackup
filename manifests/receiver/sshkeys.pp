fffb29d42be68953c13e12bbd48392fae3decfc6
mh <mh@immerda.ch>
2017-03-23 00:14:06 +0100

    this needs a bit splitting

--- manifests/receiver/sshkeys.pp
+++ manifests/receiver/sshkeys.pp
@@ -2,8 +2,9 @@
 class i2ndsitebackup::receiver::sshkeys {
   $ssh_keys = ssh_keygen("${$i2ndsitebackup::receiver::ssh_key_basepath}/i2ndsitebackup/keys/${i2ndsitebackup::receiver::source_host}/duplicity")
 
+  $split_ssh_keys = split($ssh_keys[1],' ')
   sshd::authorized_key{'sndsite':
     user  => 'sndsite',
-    key   => $ssh_keys[1],
+    key   => $split_ssh_keys[1],
   }
 }
