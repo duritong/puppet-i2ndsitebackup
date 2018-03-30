66dc89b12854c30c0a13ada46cc9fff657a62673
mh <mh@immerda.ch>
2018-03-30 21:52:16 +0200

    restore into the archive dir

--- files/restore_backup.rb
+++ files/restore_backup.rb
@@ -11,8 +11,13 @@ options = YAML.load_file('/opt/2ndsite_backup/options.yml')
 
 src=ARGV.shift
 time=ARGV.shift
-target = File.join('/tmp',File.basename(src))
-archive_dir = options['archive_dir'] ?  "--archive-dir #{options['archive_dir']} " : ''
+if options['archive_dir']
+  target = File.join(File.join(options['archive_dir'],'restore'),File.basename(src))
+  archive_dir = "--archive-dir #{options['archive_dir']} "
+else
+  target = File.join('/tmp',File.basename(src))
+  archive_dir = ''
+end
 
 puts "Starting restore..."
 old_value = ENV['PASSPHRASE']
