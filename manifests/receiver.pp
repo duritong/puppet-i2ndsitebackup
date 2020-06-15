62681e7ecb3ba61ce06cb721382d21d66bd87811
mh <mh@immerda.ch>
2020-06-15 11:18:52 +0200

    make cleanup days configureable

--- manifests/receiver.pp
+++ manifests/receiver.pp
@@ -1,8 +1,11 @@
 # things to be setup on the pushing host
 class i2ndsitebackup::receiver(
-  $user_password,
-  $source_host,
-  $ssh_key_basepath = '/etc/puppet/modules/site_securefile/files'
+  String
+    $user_password,
+  Stdlib::Unixpath
+    $ssh_key_basepath = '/etc/puppet/modules/site_securefile/files',
+  Integer
+    $cleanup_days     = 360, # (1 + 2) * 120 = (1 + fullcount) * incremental_days
 ){
 
   require rsync::client
@@ -26,19 +29,16 @@ class i2ndsitebackup::receiver(
     owner  => sndsite,
     group  => 0,
     mode   => '0600';
-  }
-
-  mount{'sndsite_disk':
+  } -> mount{'sndsite_disk':
     name    => '/srv/backup',
     device  => '/dev/mapper/2ndsite_backup',
     fstype  => 'ext4',
     options => 'defaults,noexec,nodev,noauto',
-    require => File['/srv/backup'],
   } -> file{
     '/etc/cron.monthly/backup_cleanup.sh':
-      source => 'puppet:///modules/i2ndsitebackup/cleanup_receiver.sh',
-      owner  => root,
-      group  => 0,
-      mode   => '0700',
+      content => template('i2ndsitebackup/cleanup_receiver.sh.erb'),
+      owner   => root,
+      group   => 0,
+      mode    => '0700',
   }
 }
